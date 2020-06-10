931d5cbe5ce753dbd9c84efca5ffc45c09639deb
Juloori <sjuloori@deloitte.com>
2020-06-10 18:01:21 +0530

    Platform created

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -14,9 +16,10 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+     
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +62,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
